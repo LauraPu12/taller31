0ab299b958668d572e9e84c1ce261fcf58c2412d
Laura Pulido <est.lauram.pulido@unimilitar.edu.co>
2026-04-23 10:12:34 -0500

    codigo ejemplo arbol binaario

--- src/main/java/com/mycompany/arbolbinario/ArbolBinario.java
+++ src/main/java/com/mycompany/arbolbinario/ArbolBinario.java
@@ -3,8 +3,34 @@
 package com.mycompany.arbolbinario;
 
 public class ArbolBinario {
+     // Clase Nodo (define cómo es cada nodo)
+    static class Nodo {
+        int dato; // valor que guarda el nodo
+        Nodo izquierdo; // apunta al hijo izquierdo
+        Nodo derecho; // apunta al hijo derecho
 
+        // Constructor
+        Nodo(int dato) {
+            this.dato = dato;// guarda el valor
+            this.izquierdo = null; // no tiene hijo izquierdo al inicio
+            this.derecho = null; // no tiene hijo derecho al inicio
+        }
+    }
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        Nodo raiz = new Nodo(10); // nodo principal
+
+        // Crear hijos
+        raiz.izquierdo = new Nodo(5); // hijo izquierdo de 10
+        raiz.derecho = new Nodo(20); // hijo derecho de 10
+
+        // Hijos del nodo 5
+        raiz.izquierdo.izquierdo = new Nodo(3); // hijo izquierdo de 5
+        raiz.izquierdo.derecho = new Nodo(7);   // hijo derecho de 5
+
+        // Mostrar algunos valores, sirve´para comprobar
+        System.out.println("Raíz: " + raiz.dato); // imprime 10
+        System.out.println("Izquierdo de raíz: " + raiz.izquierdo.dato); // imprime 5
+        System.out.println("Derecho de raíz: " + raiz.derecho.dato); // imprime 20
     }
 }
