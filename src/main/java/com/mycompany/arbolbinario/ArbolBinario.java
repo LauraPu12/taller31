19bb92d671c59c6ec10badc34e7ee4cdb97a66fc
Laura Pulido <est.lauram.pulido@unimilitar.edu.co>
2026-04-23 09:55:20 -0500

    se creo el proyecto

---
+++ src/main/java/com/mycompany/arbolbinario/ArbolBinario.java
@@ -0,0 +1,9 @@
+//crear el proyecto arbol binario
+package com.mycompany.arbolbinario;
+
+public class ArbolBinario {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
